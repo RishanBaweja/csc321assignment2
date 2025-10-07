95a8a9b55b6d69c7e0209367c0ec69b772dd3da2
Gary Yim <89404326+Gary055@users.noreply.github.com>
2025-10-07 01:17:32 -0700

    updated CBC from prior complete randomization issue

--- CBC.py
+++ CBC.py
@@ -2,18 +2,18 @@ from Crypto.Cipher import AES
 from os import urandom
 from operator import xor
 
-def CBC_encryption(data):
+def CBC_encryption(data, cross):
     # Using 16-byte / 128 encryption key
     key = b'Sixteen byte key'
-    randbits = urandom(16)
+    # randbits = urandom(16)
 
-    #xor data with random bits before going through the cipher
-    new_data = bytes(map(xor, data, randbits))
+    # XOR data with random bits before going through the cipher
+    new_data = bytes(map(xor, data, cross))
 
     # Cipher used with code in instructions
     cipher = AES.new(key, AES.MODE_ECB)
 
-    #encrypt the data and then return it
+    # Encrypt the data and then return it
     encrypted_data = cipher.encrypt(new_data)
 
     return encrypted_data
@@ -38,6 +38,9 @@ def file_parser(input_file):
         #move file header
         i.seek(54)
 
+        #iv created
+        prev = urandom(16)
+
         output_file = "CBC_" +input_file
         # open output file as o and write the header
         with open(output_file, "wb") as o:
@@ -49,25 +52,28 @@ def file_parser(input_file):
                 if len(data) != 16:
                     break
 
-                encrypted = CBC_encryption(data)
+                encrypted = CBC_encryption(data, prev)
+
+                # update value of prev, initially from iv then to subsequent encrypted blocks
+                prev = encrypted
 
                 o.write(encrypted)
                 
             pad = 16 - (len(data) % 16)
             data += bytes([pad]) * pad
 
-            encrypted = CBC_encryption(data)
+            encrypted = CBC_encryption(data, prev)
             o.write(encrypted)
         
 
-# file_parser("mustang.bmp")
-# file_parser("cp-logo.bmp")
+file_parser("mustang.bmp")
+file_parser("cp-logo.bmp")
 
 # pkcs7 testing
-print("\nPKCS7 padding implementation:\n")
-Unpadded = b"Something"
-Test_len = 16
-Padded = pkcs7_padding(Unpadded, Test_len)
-print(Padded)
-Undone = pkcs7_strip(Padded, Test_len)
-print(Undone)
+# print("\nPKCS7 padding implementation:\n")
+# Unpadded = b"Something"
+# Test_len = 16
+# Padded = pkcs7_padding(Unpadded, Test_len)
+# print(Padded)
+# Undone = pkcs7_strip(Padded, Test_len)
+# print(Undone)
