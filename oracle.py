0b3cc2950e816b2e4bfb54c4fb88e918609f9b34
RishanBaweja <rbaweja@calpoly.edu>
2025-10-09 10:37:37 -0700

    temp changes

--- oracle.py
+++ oracle.py
@@ -9,36 +9,39 @@ import CBC
 
 
 def submit(input : str) -> bytes:
-    #Tagging with prefix and suffix
-    mod = "userid=456; userdata=" + input + ";session-id=31337"
-
     #URL Encoding
-    url_encoded = mod.replace(";", "%3B").replace("=", "%3D")
+    url_encoded = input.replace(";", "%3B").replace("=", "%3D")
+    #Tagging with prefix and suffix
+    mod = "userid=456; userdata=" + url_encoded + ";session-id=31337"
 
-    padded = CBC.pkcs7_padding(url_encoded.encode("utf-8"), 16)
+    padded = CBC.pkcs7_padding(mod.encode("utf-8"), 16)
 
     # Typically use iv = urandom(16), but instructions said to used fixed iv
     iv = b'\xea\xbd\xf5\xe2}2\xafH\xf4\xediy\xdd\xc5\xe6\xeb'
     prev = bytes(iv)
     encrypted = b''
     for i in range(0, len(padded), 16):
-        encrypted += encrypt(padded[:16], prev)
+        block = encrypt(padded[:16], prev)
+        encrypted +=block
+        print(f"Block {i/16}: {block}\n")
         padded = padded[16:]
         prev = encrypted[i:]
+        
     return encrypted
 
 def verify(input: bytes) -> bool:
     prev = bytes(iv)
     decrypted = b''
     for i in range(0, len(input), 16):
-        decrypted += decrypt(input[i:i+16], prev)
+        block = decrypt(input[i:i+16], prev)
+        print(f"DECRYPTED Block {i/16}: {block}\n")
+        decrypted += block
         prev = input[i:i+16]
-        # print(decrypted)
 
-    print(decrypted)
+    #print(decrypted)
     unpadded = CBC.pkcs7_strip(decrypted, 16)
     plaintext = unpadded.decode("utf-8")
-    print(plaintext)
+    print(f"Plaintext: {plaintext}")
     return ";admin=true;" in plaintext
 
 def encrypt(data, cross):
@@ -62,16 +65,18 @@ cipher = AES.new(key, AES.MODE_ECB)
 # iv = urandom(16)
 iv = b'\xea\xbd\xf5\xe2}2\xafH\xf4\xediy\xdd\xc5\xe6\xeb'
 
-new_data = submit("Hey does :admin)true?")
+new_data = submit("Hey friend does ?admin?true")
 
 # use xor to tamper
-print(new_data)
+#print(f"\n\nData to tamper: {new_data}\n\n")
 
-xor_block = b'\x00' * 16
-modified_data = b''
-for i in range(0, len(new_data), 16):
-    modified_block = bytes(a ^ b for a, b in zip(new_data[i:i+16], xor_block))
-    modified_data += modified_block
+print(verify(new_data))
+
+barr = bytearray(new_data)
+print(barr)
+xor_block = bytes(ord(";") ^ ord("?"))
+barr[53] = bytes(map(xor, barr[53], xor_block))
+print(verify(bytes(barr)))
 
 # b'userid%3D456%3B userdata%3DHey does :admin)true?%3Bsession-id%3D31337\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b'
 # userid%3D456%3B 
@@ -80,4 +85,4 @@ for i in range(0, len(new_data), 16):
 # %3Bsession-id%3D
 # 31337\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b
 # There are five blocks in data without changes
-print(verify(modified_data))
+
